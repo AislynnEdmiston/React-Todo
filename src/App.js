d812203f991b7910d7699befdd68f6177f7e5c3d
AislynnEdmiston <davidandaislynn@gmail.com>
2019-10-02 19:21:33 -0400

    imported TodoList, mapped toDoList setting props

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import TodoList from './components/TodoComponents/TodoList';
 
 class App extends React.Component {
   // you will need a place to store your state in this component.
@@ -61,7 +62,10 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <h2>Welcome to your Todo App!</h2>
+        <h1>To Do List</h1>
+        <div>
+          {this.state.toDoList.map(list => <TodoList list={list}/>)}
+        </div>
       </div>
     );
   }
