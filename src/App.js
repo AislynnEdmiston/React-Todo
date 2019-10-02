abba25fbf86317aaeb10e3be58e0c9462fae807d
AislynnEdmiston <davidandaislynn@gmail.com>
2019-10-02 18:48:06 -0400

    Added state within a constructor

--- src/App.js
+++ src/App.js
@@ -4,6 +4,27 @@ class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
+
+  constructor(){
+    super();
+    this.state = {
+      item: '', 
+      toDoList: [{
+        item: 'Make todo list',
+        id: 1,
+        complete: false
+      },
+      {
+        item: 'Add new items',
+        id: 2,
+        complete: false
+      }
+    ]
+    };
+  }
+
+
+
   render() {
     return (
       <div>
